00320ff68294ca2f1e616732b3d4d8c758177a29
kkastr <11845183+kkastr@users.noreply.github.com>
2023-02-13 22:45:46 -0500

    first commit

---
+++ scraper.py
@@ -0,0 +1,51 @@
+import praw
+import pandas as pd
+from tqdm import tqdm
+from api_keys import client_id, client_secret, user_agent, username
+
+
+def getComments(url):
+
+    cols = [
+        "text",
+        "score",
+        "id",
+        "parent_id",
+        "submission_title",
+        "submission_score",
+        "submission_id"
+    ]
+
+    reddit = praw.Reddit(
+        client_id=client_id, client_secret=client_secret, user_agent=user_agent, username=username
+    )
+
+    submission = reddit.submission(url=url)
+    submission.comments.replace_more(limit=0)
+    rows = []
+
+    for comment in submission.comments.list():
+
+        if comment.stickied:
+            continue
+
+        data = [
+            comment.body,
+            comment.score,
+            comment.id,
+            comment.parent_id,
+            submission.title,
+            submission.score,
+            submission.id,
+        ]
+
+        rows.append(data)
+
+    df = pd.DataFrame(data=rows, columns=cols)
+
+    df.to_csv(f'{submission.id}_comments.csv', index=False)
+
+    return df
+
+if __name__ == "__main__":
+    pass
